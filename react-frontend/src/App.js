86d90bb0da327860d78d0447e5223b47caf9feab
ArchanaVyshak <archanamja@gmail.com>
2023-01-08 02:10:32 +0530

    initial

---
+++ react-frontend/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import ListEmployeeComponent from './components/ListEmployeeComponent';
+import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
+import CreateEmployeeComponent from './components/CreateEmployeeComponent';
+
+function App() {
+  return (
+    <div>
+      <Router>
+          <HeaderComponent />
+            <div className="container">
+              <Switch>
+                <Route path="/" exact component={ListEmployeeComponent}></Route>
+                <Route path="/employees" exact component={ListEmployeeComponent}></Route>
+                <Route path="/add-employee" exact component={CreateEmployeeComponent}></Route>
+              </Switch>
+            </div>
+          <FooterComponent />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
